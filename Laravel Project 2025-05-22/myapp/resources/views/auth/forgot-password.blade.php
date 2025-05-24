6bfb2c367ce82d3a7e39358aa339f1b3cbd2eee4
Freelancer Daud <freelancerdaud0@gmail.com>
2025-05-24 22:06:24 +0600

    add Logo svg code

--- Laravel Project 2025-05-22/myapp/resources/views/auth/forgot-password.blade.php
+++ Laravel Project 2025-05-22/myapp/resources/views/auth/forgot-password.blade.php
@@ -30,5 +30,11 @@
                 </x-button>
             </div>
         </form>
+         <hr class="mt-4">
+        <div class="flex items-center justify-center mt-4">
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                    {{ __("Login?") }}
+                </a>
+        </div>
     </x-authentication-card>
 </x-guest-layout>
